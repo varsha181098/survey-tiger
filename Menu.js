f2b83032c002fca8a0f4741d7df9e00a9fff9269
varsha181098 <118925989+varsha181098@users.noreply.github.com>
2023-01-03 23:52:45 +0530

    Create Menu.js

---
+++ Menu.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Link}  from 'react-router-dom';
+
+const Menu = () => {
+    return (
+        <div className="my-3">
+            <Link to="/create">
+            <button className="btn btn-danger m-1">Create Survey</button>
+            </Link>
+            <Link to="/published">
+            <button className="btn btn-danger m-1">Take Survey</button>
+            </Link>
+
+        </div>
+    );
+};
+
+export default Menu;
